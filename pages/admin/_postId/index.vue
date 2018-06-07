93fb98e64a3417a369c34fab4a664f3aa77c03ed
dipto <lm33120@gmail.com>
2018-06-07 16:22:21 +0600

    post list

---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost"/>
+        </section>
+    </div>
+</template>
+<script>
+    import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+    export default {
+        components:{
+            AdminPostForm
+        },
+        data()
+        {
+          return{
+            loadedPost:{
+              author:'Maximilian',
+              title:'My awesome post',
+              content:'Super amazing',
+              thumbnailLink:'lol'
+            }
+          }
+        }
+
+    }
+</script>
---
+++ pages/posts/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="posts-page">
+      <PostList/>
+    </div>
+</template>
+<script scoped>
+    import PostList from '@/components/Posts/PostList'
+    export default {
+        components:{
+            PostList
+        }
+    }
+</script>
+<style scoped>
+    .posts-page{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .post-list {
+        display: flex;
+        padding: 20px;
+        box-sizing: border-box;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+    }
+
+</style>
